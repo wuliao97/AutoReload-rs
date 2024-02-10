1a53b5759d3dc34714eee3be8c7ca3e7b093a770
wuliao97 <117802193+wuliao97@users.noreply.github.com>
2024-02-10 10:19:29 +0900

    b

--- src/ar.rs
+++ src/ar.rs
@@ -1,11 +1,14 @@
 use tokio::sync::Mutex;
 use std::time::Duration;
 use async_trait::async_trait;
+use daemonize::Daemonize;
+use daemonaize;
 
+pub type Error = Box<dyn std::error::Error + Sync + Send>;
 
 #[async_trait]
 pub trait ARTrait {
-    async fn execute(&self) -> Result<(), Box<dyn std::error::Error + Sync + Send>>;
+    async fn execute(&self) -> Result<(), Error>;
 }
 
 pub struct AR<T: ARTrait> {
@@ -14,12 +17,19 @@ pub struct AR<T: ARTrait> {
 }
 impl<T: ARTrait> AR<T> {
     pub fn new(obj: T, secs: u64) -> Self {
+        env_logger::init();
+
         let obj = Mutex::new(obj);
         let span = Duration::from_secs(secs);
         Self {
             obj, span
         }
     }
+
+    pub fn new_thirty_mins(obj: T) -> Self {
+        let secs = 60 * 30;
+        AR::new(obj, secs)
+    } 
 
     pub fn new_one_hour(obj: T) -> Self {
         let secs = 60 * 60;
@@ -28,8 +38,24 @@ impl<T: ARTrait> AR<T> {
 
 
     pub async fn start(&self) -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
+        let x = self.obj.lock().await;
 
-    
+        log::debug!("Starting to Auto Reload");
+        println!("test");
+
+
+        let daemonize = Daemonize::new()
+            .privileged_action(|| return "Executed before drop privileges");
+
+        match daemonize.start() {
+            Ok(_) => {
+                x.execute().await;
+                log::debug!("SUCCESS")
+            }
+            Err(err) => {
+                log::error!("{:#?}", err)
+            }
+        }
 
         Ok(())
     }
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,30 @@ pub mod ar;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::ar::{ARTrait, AR, Error};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    struct Person {
+        name: String
+    }
+    impl Person {
+        fn self_introduce(&self) {
+            println!("My name is ...{}", self.name);
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl ARTrait for Person {
+        async fn execute(&self) -> Result<(), Error> {
+            self.self_introduce();
+            Ok(())
+        } 
+    }
+
+    #[tokio::test]
+    async fn it_works() {
+        let p = Person { name: String::from("Ennui") };
+        let ar = AR::new(p, 2);
+        println!("Start");
+        ar.start().await.ok();
     }
 }
